ec61deb7cdfbf1ba7b6a76b6ef58e9fcb4b249ed
Andrej Naumovski <andrejnaumovski@Andrejs-MacBook-Pro.local>
2017-06-28 22:23:56 +0200

    [CONFIG] Configured Logback logging for development and production environments

--- api/src/main/java/mk/edu/ukim/feit/bolt/api/BoltApiApplication.java
+++ api/src/main/java/mk/edu/ukim/feit/bolt/api/BoltApiApplication.java
@@ -3,6 +3,8 @@ package mk.edu.ukim.feit.bolt.api;
 import mk.edu.ukim.feit.bolt.api.controllers.ControllerScanMarker;
 import mk.edu.ukim.feit.bolt.api.repositories.RepositoryScanMarker;
 import mk.edu.ukim.feit.bolt.api.services.ServiceScanMarker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,7 +19,12 @@ import org.springframework.context.annotation.Configuration;
 })
 public class BoltApiApplication {
 
+    static final Logger logger = LoggerFactory.getLogger(BoltApiApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BoltApiApplication.class, args);
+        logger.info("Logging successfully initialized!");
+        logger.error("Oops, an error occurred!");
+        logger.debug("Debugging error!");
 	}
 }
