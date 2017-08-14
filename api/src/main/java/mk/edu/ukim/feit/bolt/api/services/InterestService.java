db46090be6c51ce004b5942179200187a0d9f713
Gjorgji Markov <gjorgjimarkov@yahoo.com>
2017-08-14 20:34:16 +0200

    [API] Add interest service and repository.

---
+++ api/src/main/java/mk/edu/ukim/feit/bolt/api/services/InterestService.java
@@ -0,0 +1,14 @@
+package mk.edu.ukim.feit.bolt.api.services;
+
+import mk.edu.ukim.feit.bolt.api.models.Interest;
+
+import java.util.List;
+
+/**
+ * Created by gjorgjim on 8/14/17.
+ */
+public interface InterestService {
+    Interest findByName(String name);
+    Interest findById(Long id);
+    List<Interest> findAll();
+}
---
+++ api/src/main/java/mk/edu/ukim/feit/bolt/api/services/impl/InterestServiceImpl.java
@@ -0,0 +1,36 @@
+package mk.edu.ukim.feit.bolt.api.services.impl;
+
+import mk.edu.ukim.feit.bolt.api.models.Interest;
+import mk.edu.ukim.feit.bolt.api.repositories.InterestRepository;
+import mk.edu.ukim.feit.bolt.api.services.InterestService;
+
+import java.util.List;
+
+/**
+ * Created by gjorgjim on 8/14/17.
+ */
+public class InterestServiceImpl implements InterestService {
+    private InterestRepository interestRepository;
+
+    public InterestServiceImpl(InterestRepository interestRepository) {
+        if(interestRepository == null) {
+            throw new IllegalArgumentException(InterestRepository.class.getName() + " cannot be null");
+        }
+        this.interestRepository = interestRepository;
+    }
+
+    @Override
+    public Interest findByName(String name) {
+        return interestRepository.findByName(name);
+    }
+
+    @Override
+    public Interest findById(Long id) {
+        return interestRepository.findOne(id);
+    }
+
+    @Override
+    public List<Interest> findAll() {
+        return (List<Interest>)interestRepository.findAll();
+    }
+}
